2687adbd5c85688cf78d67eed8160ce8267a164b
laristizabal <laristizabal@summan.com>
2026-03-08 21:36:26 -0500

    Se crea app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import os
 import streamlit as st
+import joblib
 
-path = "models/ods_pipeline.joblib"
+modelo = joblib.load("models/ods_pipeline.joblib")
 
-st.write("Existe:", os.path.exists(path))
-if os.path.exists(path):
-    st.write("Tamaño bytes:", os.path.getsize(path))
-    with open(path, "rb") as f:
-        first_bytes = f.read(80)
-    st.write("Primeros bytes:", first_bytes)
+st.title("Clasificador de ODS")
+
+texto = st.text_area("Ingrese un texto")
+
+if st.button("Clasificar"):
+    pred = modelo.predict([texto])[0]
+    st.write(f"ODS predicho: {pred}")
